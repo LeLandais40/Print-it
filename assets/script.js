5a367afaff2a1a6a0c5ed977169ac65dc33aae49
LeLandais40 <dacquoisud40@gmail.com>
2023-09-26 18:07:07 +0200

    Print-it! fini

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,36 @@
-const slide = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+let data = [
+	{image: 'assets/images/slideshow/slide1.jpg', text: 'Impressions tous formats <span>en boutique et en ligne</span>'},
+	{image: 'assets/images/slideshow/slide2.jpg', text: 'Tirages haute définition grand format <span>pour vos bureaux et events</span>'},
+	{image: 'assets/images/slideshow/slide3.jpg', text: 'Grand choix de couleurs <span>de CMJN aux pantones</span>'},
+	{image: 'assets/images/slideshow/slide4.png', text: 'Autocollants <span>avec découpe laser sur mesure</span>'},
+];
+
+let index = 1;
+
+function updateSlider() {
+	let imgElement = document.getElementById('slide');
+	let textElement = document.querySelector('#banner p');
+
+	imgElement.src = data[index].image;
+	textElement.innerHTML = data[index].text;
+
+	const bulletspoint = document.querySelector(".dots");
+	bulletspoint.innerHTML = "";
+	for (let i = 0; i < data.length; i++) {
+		const bullet = document.createElement("p");
+		bullet.classList.add("dot");
+		if (i == index) {
+			bullet.classList.add("dot_selected");
+		}
+		bulletspoint.appendChild(bullet);
 	}
-]
-
-let numero = 0;
-
-function ChangeSlide(sens) {
-    numero = numero + sens;
-    if (numero < 0)
-        numero = slide.length - 1;
-    if (numero > slide.length - 1)
-        numero = 0;
-    document.getElementById("slide").src = slide[numero];
 }
+
+function ChangeSlide(n) {
+    index += n;
+	if (index === data.length) index = 0;
+	if (index < 0) index = data.length - 1;
+	updateSlider();
+}
+
+updateSlider();
